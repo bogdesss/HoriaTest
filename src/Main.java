2134313c2babb485412c08ddee668919d364c34a
codomsa <domsacosmin@gmail.com>
2023-10-07 15:39:43 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world Marcu!");
-        System.out.println("Hello world Bogdan!");
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("First name:");
+        String firstName = in.next();
+        System.out.println("Last name:");
+        String lastName=in.next();
+        System.out.println("Hello," + firstName + " " + lastName + "!");
     }
 }
